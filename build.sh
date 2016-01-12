b48a5ba34641c2375899b0507db22f643ffa5ab8
bmaupin <bmaupin@users.noreply.github.com>
2016-01-12 15:12:34 -0500

    Use a temporary local install of musl

--- build.sh
+++ build.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
+# Get latest Musl version from http://www.musl-libc.org/download.html
+musl_version=1.1.12
+# Get latest OpenLDAP version from http://www.openldap.org/software/download/
+openldap_version=2.4.43
+
+
+
 # Install dependencies
 sudo apt-get -y install libssl-dev
 
-# Get latest Musl version from http://www.musl-libc.org/download.html
-export musl_version=1.1.12
+# Build musl
 wget http://www.musl-libc.org/releases/musl-$musl_version.tar.gz
 tar -xvf musl-$musl_version.tar.gz
 cd musl-$musl_version
-./configure --prefix=/usr/local  --disable-shared
+musl_install=`pwd`/local
+# --disable-shared is necessary because for some reason otherwise the OpenLDAP tools have a dependency on libc.so
+# --enable-wrapper=gcc builds the musl-gcc binary wrapper
+./configure --prefix=$musl_install --disable-shared --enable-wrapper=gcc
 make
-sudo make install
+make install
 cd ..
 
-# Cleanup
-rm -rf musl-$musl_version
-rm musl-$musl_version.tar.gz
-
-# Get latest OpenLDAP version from http://www.openldap.org/software/download/
-export openldap_version=2.4.43
+# Build openldap
 wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-$openldap_version.tgz
 tar -xvf openldap-$openldap_version.tgz
 cd openldap-$openldap_version
-CC="musl-gcc" LDFLAGS="-static" ./configure --prefix=/usr/local --disable-shared --disable-slapd
+CC="$musl_install/bin/musl-gcc" LDFLAGS="-static" ./configure --prefix=/usr/local --disable-shared --disable-slapd
 make depend
 make
 
@@ -30,5 +34,7 @@ make
 cd clients; find tools/ -executable -type f | tar -cvzf ../../openldap-tools-$openldap_version-`uname -p`.tgz -T -; cd ../..
 
 # Cleanup
+rm -rf musl-$musl_version
+rm musl-$musl_version.tar.gz
 rm -rf openldap-$openldap_version
 rm openldap-$openldap_version.tgz
